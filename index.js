d1666dedf60f929c14f7ae7a2769d97b29cf3f52
Thiago Henrique <thiagohenriquedev@gmail.com>
2021-11-22 07:18:04 -0200

    up

--- index.js
+++ index.js
@@ -10,6 +10,19 @@ var admin = document.querySelector("#exampleInputAdmin")
 var fields = document.querySelectorAll("#form-user-create [name]");
 
     fields.forEach(function(field, index){
+        
+        if(field.name == "gender"){
+
+            if (field.checked){
+            console.log("SIM", field)
+        }
+            
+
+        } else {
+
+            console.log("NÃO")
+
+        }
 
         console.log(field.id, field.name, field.value, field.checked, index)
 
