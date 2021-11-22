c65ba2af1f3d18681c2a820287b3fe60a620aef2
thiago-henrique-dev <thiagohenriquedev@gmaail.com>
2021-11-22 14:23:54 -0300

    up

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -15,11 +15,44 @@ class UserController {
 
             event.preventDefault();
 
-           let user =  this.getValues();
+            let values = this.getValues();
 
-           this.addLine(user, "")
+            
+            this.getPhoto((content)=>{
+
+                values.photo = content;
+
+                this.addLine(values)
+
+            });
+
+            
     
-        });
+            });
+
+        }
+
+        getPhoto(callback){
+
+            let fileReader = new FileReader();
+
+            let elements = [...this.formEl.elements].filter(item=>{
+
+                if(item.name === 'photo'){
+                    return item 
+                }
+
+            })
+
+            let file = elements[0].files[0]
+
+            fileReader.onload = ()=>{
+
+                fileReader.result
+                callback(fileReader.result);
+
+            }
+            fileReader.readAsDataURL(file);
         }
 
 
@@ -63,7 +96,7 @@ class UserController {
         
         this.tableEl.innerHTML = `
         <tr>
-        <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+        <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
         <td>${dataUser.name}</td>
         <td>${dataUser.email}</td>
         <td>${dataUser.admin}</td>
